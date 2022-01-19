eb6ec1d1aa903cf99b0063f9654dbaf583405795
Jayati Mishra <jmishra_be20@thapar.edu>
2022-01-19 11:57:05 +0530

    Heroku is there

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,13 +1,7 @@
-from my_app import views
-from django.urls import path, include 
-from django.contrib import admin
+from django.urls import path
+from . import views
 
-
-urlpatterns=[
-    path('',views.home, name='home_page'),
-
-    path('css/', include('my_app.urls')),
-    path('new-search',views.new_search,name-'new_search'),
-
-
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search'),
 ]
